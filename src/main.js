746cb7135a97e9b12af6f8348392e69d259e4d22
Carla Ros <krlarosm@gmail.com>
2020-04-22 16:03:34 +0100

    "install ckeditor"

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import CKEditor from '@ckeditor/ckeditor5-vue'
 
-Vue.config.productionTip = false;
+Vue.use(CKEditor)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
